5d7c30a259af0a6c94505966e5d04bafac3e16db
treks1998 <treks1998@gmail.com>
2019-11-23 14:02:17 +0300

    123

--- src/slider.js
+++ src/slider.js
@@ -12,8 +12,6 @@ import logo10 from './images/10.jpg';
 import logo11 from './images/11.jpg';
 import logo12 from './images/12.jpg';
 import logo13 from './images/13.jpg';
-import logo14 from './images/14.jpg';
-import logo15 from './images/15.jpg';
 import  './style.css';
 import Header from './header';
 import  {data as text}  from './data';
@@ -22,7 +20,7 @@ class Slider extends React.Component {
     state={
         currentImages:{},
         allText:[],
-        currentText:'текст первого слайда' ,
+        currentText:'' ,
 }
 
 componentDidMount() {
@@ -56,10 +54,6 @@ nextImages=()=>{
             break;
         case logo12:this.setState({ currentImages: logo13,  currentText:this.state.allText[12] });
             break;
-        case logo13:this.setState({ currentImages: logo14,  currentText:this.state.allText[13] });
-            break;
-        case logo14:this.setState({ currentImages: logo15,  currentText:this.state.allText[14] });
-            break;
         default:
             break;
     }
@@ -90,87 +84,17 @@ nextImages=()=>{
                 break;
             case logo13:this.setState({ currentImages: logo12,  currentText:this.state.allText[11] });
                 break;
-            case logo14:this.setState({ currentImages: logo13,  currentText:this.state.allText[12] });
-                break;
-            case logo15:this.setState({ currentImages: logo14,  currentText:this.state.allText[13] });
-                break;
             default:
                 break;
         }
     };
 
     handleKeyPress = (event) => {
-        if(event.charCode == '90'){
-            switch(this.state. currentImages) {
-                case logo2:this.setState({ currentImages: logo1,  currentText:this.state.allText[0] });
-                    break;
-                case logo3:this.setState({ currentImages: logo2,  currentText:this.state.allText[1] });
-                    break;
-                case logo4:this.setState({ currentImages: logo3,  currentText:this.state.allText[2] });
-                    break;
-                case logo5:this.setState({ currentImages: logo4,  currentText:this.state.allText[3] });
-                    break;
-                case logo6:this.setState({ currentImages: logo5,  currentText:this.state.allText[4] });
-                    break;
-                case logo7:this.setState({ currentImages: logo6,  currentText:this.state.allText[5] });
-                    break;
-                case logo8:this.setState({ currentImages: logo7,  currentText:this.state.allText[6] });
-                    break;
-                case logo9:this.setState({ currentImages: logo8,  currentText:this.state.allText[7] });
-                    break;
-                case logo10:this.setState({ currentImages: logo9,  currentText:this.state.allText[8] });
-                    break;
-                case logo11:this.setState({ currentImages: logo10,  currentText:this.state.allText[9] });
-                    break;
-                case logo12:this.setState({ currentImages: logo11,  currentText:this.state.allText[10] });
-                    break;
-                case logo13:this.setState({ currentImages: logo12,  currentText:this.state.allText[11] });
-                    break;
-                case logo14:this.setState({ currentImages: logo13,  currentText:this.state.allText[12] });
-                    break;
-                case logo15:this.setState({ currentImages: logo14,  currentText:this.state.allText[13] });
-                    break;
-                default:
-                    break;
-            }
-        }
-        if(event.charCode == '88'){
-            switch(this.state. currentImages) {
-                case logo1:this.setState({ currentImages: logo2,  currentText:this.state.allText[1] });
-                    break;
-                case logo2:this.setState({ currentImages: logo3,  currentText:this.state.allText[2] });
-                    break;
-                case logo3:this.setState({ currentImages: logo4,  currentText:this.state.allText[3] });
-                    break;
-                case logo4:this.setState({ currentImages: logo5,  currentText:this.state.allText[4] });
-                    break;
-                case logo5:this.setState({ currentImages: logo6,  currentText:this.state.allText[5] });
-                    break;
-                case logo6:this.setState({ currentImages: logo7,  currentText:this.state.allText[6] });
-                    break;
-                case logo7:this.setState({ currentImages: logo8,  currentText:this.state.allText[7] });
-                    break;
-                case logo8:this.setState({ currentImages: logo9,  currentText:this.state.allText[8] });
-                    break;
-                case logo9:this.setState({ currentImages: logo10,  currentText:this.state.allText[9] });
-                    break;
-                case logo10:this.setState({ currentImages: logo11,  currentText:this.state.allText[10] });
-                    break;
-                case logo11:this.setState({ currentImages: logo12,  currentText:this.state.allText[11] });
-                    break;
-                case logo12:this.setState({ currentImages: logo13,  currentText:this.state.allText[12] });
-                    break;
-                case logo13:this.setState({ currentImages: logo14,  currentText:this.state.allText[13] });
-                    break;
-                case logo14:this.setState({ currentImages: logo15,  currentText:this.state.allText[14] });
-                    break;
-                default:
-                    break;
-            }
+        if(event.charCode === 'Enter'){
+          console.log('111111111111111');
         }
     };
     render() {
-        console.log('this.state.allText',this.state.allText);
         return (
            <div onKeyPress={this.handleKeyPress}>
                 <Header/>
